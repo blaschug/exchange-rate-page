68f3cd466d40b3724c61a5a5f499986df7361e55
Tomas Capretto <tomicapretto@gmail.com>
2021-09-12 22:06:00 -0300

    Cambio nombres, agrego algunos chequeos, y elimino otros

--- src/scripts/jsExchange.js
+++ src/scripts/jsExchange.js
@@ -1,66 +1,53 @@
-const $boton = $("#boton");
-$boton[0].onclick = pares;
+const monto = $("#monto");
+const precios = $("#precios");
+const boton = $("#boton");
+const monedaBase = $("#moneda-base");
+const fecha = $("#fecha");
+boton.on("click", obtenerPares);
 
-function pares(e) {
-  if (validaFormulario()) {
-    muestraResultados();
-    fetch(
-      `https://api.exchangerate.host/${$("#selecciona-fecha")[0].value}?base=${
-        $("#seleccion")[0].value
-      }&symbols=${devuelveSimbolos()}&amount=${monto()}`
-    )
+function obtenerPares(e) {
+  if (validarFormulario()) {
+    mostrarResultados();
+    fetch(crearURL(fecha.val(), monedaBase.val(), monto.val()))
       .then((respuesta) => respuesta.json())
       .then((respuestaJSON) => {
-        $("#precios").text(
+        precios.text(
           `Cambios del dia ${respuestaJSON.date} en base ${respuestaJSON.base}`
         );
         Object.keys(respuestaJSON.rates).forEach((moneda) => {
-          $("#precios").append(
+          precios.append(
             $(
               `<li><strong>${moneda}</strong>: ${respuestaJSON.rates[moneda]}</li>`
             )
           );
         });
       })
-      .catch((error) => console.error("Fallo:", error));
+      .catch((error) => {
+        console.error("Fallo:", error)
+      });
     e.preventDefault();
   }
   e.preventDefault();
 }
 
-function muestraResultados() {
-  const $precios = $("#precios");
-  $precios.removeClass("is-hidden");
+
+function mostrarResultados() {
+  precios.removeClass("is-hidden");
 }
 
-function monto() {
-  let monto = $("#monto")[0].value;
-  if (monto) {
-    return monto;
-  } else {
-    return 1;
-  }
-}
-
-function devuelveSimbolos() {
+function obtenerSimbolos() {
   let pares = "";
-  let cantidadPares = $("#pares");
-  let asd = document.querySelectorAll(".elegir");
-  for (let i = 0; i < asd.length; i++) {
-    if (asd[i].checked) {
-      let palabra = cantidadPares[0].children[i + 1].innerText.slice(1, 4);
+  const paresContainer = $("#pares-container");
+  const inputs = document.querySelectorAll(".elegir");
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].checked) {
+      let palabra = paresContainer[0].children[i + 1].innerText.slice(1, 4);
       pares = pares + `${palabra},`;
     }
   }
   return pares.slice(0, -1);
 }
 
-function validaFormulario() {
-  let fecha = validadorFecha();
-  let cambio = validadoTipoCambio();
-  let pares = validaPares();
-  if (fecha && cambio && pares) {
-    return true;
-  }
-  return false;
+function crearURL(fecha, base, monto) {
+  return `https://api.exchangerate.host/${fecha}?base=${base}&symbols=${obtenerSimbolos()}&amount=${monto}`
 }
